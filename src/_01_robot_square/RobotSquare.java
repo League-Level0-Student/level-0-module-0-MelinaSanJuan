5abb71b844d6fb32445079edfa241fd776196996
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-31 13:30:46 -0700

    RobotSquare Robot Graffiti(IP)

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -4,6 +4,8 @@ package _01_robot_square;
  *    Level 0
  */
 
+import javax.swing.plaf.FontUIResource;
+
 import org.jointheleague.graphical.robot.Robot;
 
 // This recipe draws a square using the Robot
@@ -12,15 +14,19 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot sofia = new Robot();
 
         // 3. Put the robot's pen down
-
+sofia.penDown();
 
         // 6. Make the robot move as fast as possible
+sofia.setSpeed(10);
 
+        // 5. Do everything below here 4 time.
+for(int i = 0; i <4; i++) {
+sofia.move(200);
+sofia.turn(90);}
 
-        // 5. Do everything below here 4 times
 
 
         //         2. Move your robot 200 pixels
@@ -30,4 +36,9 @@ public class RobotSquare {
 
 
     }
+
+	private static void move(int i) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -0,0 +1,27 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+
+	public static void main(String[] args) {
+		
+	Robot melina = new Robot ();
+melina.penDown();
+melina.setSpeed(10);
+melina.move(125);
+melina.turn(90);
+melina.move(125);
+
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	}	
+}
